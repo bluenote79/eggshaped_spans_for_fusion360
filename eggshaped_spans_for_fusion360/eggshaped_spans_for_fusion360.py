e3682c11bbb5c717ca7fce8976a1aeabf0ffd905
bluenote79 <joerg@niederhoff.com>
2024-10-13 10:54:04 +0200

    Update eggshaped_spans_for_fusion360.py
    New menu structure

--- eggshaped_spans_for_fusion360/eggshaped_spans_for_fusion360.py
+++ eggshaped_spans_for_fusion360/eggshaped_spans_for_fusion360.py
@@ -3,10 +3,17 @@ import math as m
 
 """
 This skript adds spans calculated by Hügelschäffer equation to Fusion 360 fitting 3 splines ready for lofting.
+V2 Menues structured
 """
 
 handlers = []
+
+
+
+
 ui = None
+rowNumber = 0
+
 app = adsk.core.Application.get()
 if app:
     ui  = app.userInterface
@@ -23,60 +30,103 @@ l_ymax = []
 l_off = []
 l_Plane = []
 
+val_ids_list =[]
+
+COMMAND_ID = "SpanCMDDef"
 INPUT_ORIENTIERUNG = ""
 SE01_SELECTION_INPUT_ID = "Spline oben"
 SE02_SELECTION_INPUT_ID = "Spline unten"
 SE03_SELECTION_INPUT_ID = "Spline mitte"
-IN01_VALUE_INPUT_ID = "Anzahl Punkte"
+IN01_VALUE_INPUT_ID = "Abstand bei gleichem Abstand"
 SE04_SELECTION_INPUT_ID = "Parallele Ursprungsebene"
 IN02_VALUE_INPUT_ID = "Abstand Spanten"
 IN03_VALUE_INPUT_ID ='Anzahl Spanten gleichen Abstands'
 CH01_BOOLEAN_INPUT_ID = 'checkbox'
-IN04_STRING_INPUT_ID = "Spanten semikolngetrennt"
+CH02_BOOLEAN_INPUT_ID = 'checkbox2'
+
+
+
+# Adds a new row to the table.
+def addRowToTable(tableInput):
+    global rowNumber, val_ids_list
+
+    cmdInputs = adsk.core.CommandInputs.cast(tableInput.commandInputs)
+    
+    valueInput = cmdInputs.addValueInput('TableInput_value{}'.format(rowNumber), 'Value', 'mm', adsk.core.ValueInput.createByReal(0.0))
+    val_ids_list.append((f'TableInput_value{rowNumber}', rowNumber, valueInput.value))         # ids to read values later
+    row = tableInput.rowCount
+    tableInput.addCommandInput(valueInput, row, 0)
+    
+    # Increment a counter used to make each row unique.
+    rowNumber = rowNumber + 1
+
+
+# Event handler that reacts to any changes the user makes to any of the command inputs.
+class MyCommandInputChangedHandler(adsk.core.InputChangedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            eventArgs = adsk.core.InputChangedEventArgs.cast(args)
+            inputs = eventArgs.inputs
+            cmdInput = eventArgs.input
+
+            global abstand, val_ids_list
+            if cmdInput.id == IN02_VALUE_INPUT_ID:
+                abstand = inputs.itemById(IN02_VALUE_INPUT_ID)
+           
+            tableInput = inputs.itemById('table')
+            if cmdInput.id == 'tableAdd':
+                addRowToTable(tableInput)
+            elif cmdInput.id == 'tableDelete':
+                if tableInput.selectedRow == -1:
+                    ui.messageBox('Select one row to delete.')
+                else:              
+
+                    del val_ids_list[int(tableInput.selectedRow)]
+                    tableInput.deleteRow(tableInput.selectedRow)
+          
+        except:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+
+
 
 class SpanCommandExecuteHandler(adsk.core.CommandEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
         try:
+            
             command = args.firingEvent.sender
             inputs = command.commandInputs
-            input1 = inputs[0]
-            sel0 = input1.selection(0)
-            input2 = inputs[1]
-            sel1 = input2.selection(0)
-            input3 = inputs[2]
-            sel2 = input3.selection(0)
-            input4 = inputs[3]     # number of points / 4
-            input5 = inputs[4]   #plane
-            sel3 = input5.selection(0)
-            input6 = inputs[5]     # abstand
-            input7 = inputs[6]      # number of spans
-            input8 = inputs[7]     
-            input9 = inputs[8]    # weitere Spanten mit offsetwert angeben
 
-            anzahl_span = float(input7.value)
 
-            delta_span = float(input6.value)
+            sel0 = inputs.itemById(SE01_SELECTION_INPUT_ID).selection(0)
+            sel1 = inputs.itemById(SE02_SELECTION_INPUT_ID).selection(0)
+            sel2 = inputs.itemById(SE03_SELECTION_INPUT_ID).selection(0)
+            sel3 = inputs.itemById(SE04_SELECTION_INPUT_ID).selection(0)
+            input4 = inputs.itemById(IN01_VALUE_INPUT_ID) # points / 4
+            delta_span = float((inputs.itemById(IN02_VALUE_INPUT_ID)).value) # distance
+            anzahl_span = float((inputs.itemById(IN03_VALUE_INPUT_ID).value))  # number of spans
 
-            if input9.value != "":
-                temp = input9.value
-                temp = temp.split(";")
-                temp = list(map(int, temp))
-                for i in range(len(temp)):
-                    l_off.append(float(temp[i]/10))
 
-            if input8 == True:
+            if inputs.itemById(CH01_BOOLEAN_INPUT_ID) == True:
                 faktor = i
                 anzahl_span -= 1
 
+            if (inputs.itemById(CH02_BOOLEAN_INPUT_ID)).value == True:
+                dir_off = -1
+            else:
+                dir_off = 1
+
             for i in range(int(anzahl_span)):
-                if input8.value == True:
+                if (inputs.itemById(CH01_BOOLEAN_INPUT_ID)).value == True:
                     faktor = i
                 else:
                     faktor = i + 1
                 temp = float(faktor * delta_span)
-                l_off.append(temp)
+                l_off.append(dir_off * temp)
     
             if sel3.entity == root.xYConstructionPlane:
                 INPUT_ORIENTIERUNG = "xY"
@@ -100,7 +150,7 @@ class SpanCommandExecuteHandler(adsk.core.CommandEventHandler):
             entities_u.append(spline_u)
             entities_m.append(spline_m)
             
-            entities_all = entities_o + entities_u + entities_m                ################## del?
+            entities_all = entities_o + entities_u + entities_m  # del?
 
             def intersection_ebene(offset_z, entities_o, entities_u, entities_m, INPUT_ORIENTIERUNG):
 
@@ -214,14 +264,28 @@ class SpanCommandExecuteHandler(adsk.core.CommandEventHandler):
                 letzte_ebene = len(planes) - 1
                 planes[letzte_ebene].deleteMe()
 
+            
+            # val_ids_list get rid of dublets add append l_off
+            def prepare_val_ids():
+                global val_ids_list
+
+                names_ids = [val_ids_list[i][0] for i in range(len(val_ids_list))]
+                values = [float(inputs.itemById(names_ids[i]).value) for i in range(len(names_ids))]
+                cleaned_vals = list(set(values))
+                for i in range(len(cleaned_vals)):
+                    l_off.append( float(cleaned_vals[i]))
+
+            prepare_val_ids()
+            
             for i in range(len(l_off)):
                 intersection_ebene(l_off[i], entities_o, entities_u, entities_m, INPUT_ORIENTIERUNG)
 
+            
 
             # execute the calculation programm with the user selected values
             span = Span()
             for i in range(len(l_off)):
-                span.Execute(l_xmax[i], l_xmin[i], l_xmit[i], l_ymax[i], 4 * input4.value, l_off[i], INPUT_ORIENTIERUNG)
+                span.Execute(l_xmax[i], l_xmin[i], l_xmit[i], l_ymax[i], 4 * input4.value, -1 * l_off[i], INPUT_ORIENTIERUNG)
 
         except:
             if ui:
@@ -351,52 +415,102 @@ class Span:
 class SpanCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
         super().__init__()
-    def notify(self, args):
+    def notify(self, args: adsk.core.CommandEventArgs):
         try:
 
-            # Get the command that was created.
-            cmd = adsk.core.Command.cast(args.command)
-
             onExecute = SpanCommandExecuteHandler()
-            cmd.execute.add(onExecute)
+            args.command.execute.add(onExecute)
             handlers.append(onExecute) 
 
-            # Connect to the command destroyed event.
             onDestroy = SpanCommandDestroyHandler()
-            cmd.destroy.add(onDestroy)
+            args.command.destroy.add(onDestroy)
             handlers.append(onDestroy)
 
-            # Get the CommandInputs collection associated with the command.
-            inputs = cmd.commandInputs
+            onInputChanged = MyCommandInputChangedHandler()
+            args.command.inputChanged.add(onInputChanged)
+            handlers.append(onInputChanged)  
 
-            # Create the inputs       
 
-            i1 = inputs.addSelectionInput(SE01_SELECTION_INPUT_ID, SE01_SELECTION_INPUT_ID, "Select curve")
+            inputs = args.command.commandInputs
+
+            tabCmdInput1 = inputs.addTabCommandInput('tab_1', 'Settings')
+            tab1ChildInputs = tabCmdInput1.children
+
+
+
+            groupCmdInput = tab1ChildInputs.addGroupCommandInput('group', 'Auswahl der Splines und Projektionsebene:')
+            groupCmdInput.isExpanded = True
+            groupCmdInput.isEnabledCheckBoxDisplayed = False
+            groupChildInputs = groupCmdInput.children
+
+
+            group2CmdInput = tab1ChildInputs.addGroupCommandInput('group2', 'Eiform Punkte pro Quadrant:')
+            group2CmdInput.isExpanded = False
+            group2CmdInput.isEnabledCheckBoxDisplayed = False
+            group2ChildInputs = group2CmdInput.children
+            
+
+            group3CmdInput = tab1ChildInputs.addGroupCommandInput('group3', 'Spanten gleichen Abstands erzeugen:')
+            group3CmdInput.isExpanded = False
+            group3CmdInput.isEnabledCheckBoxDisplayed = False
+            group3ChildInputs = group3CmdInput.children
+
+            group4CmdInput = tab1ChildInputs.addGroupCommandInput('group3', 'Spanten unterschiedlichen Abstands zur Usrprungsebene erzeugen:')
+            group4CmdInput.isExpanded = True
+            group4CmdInput.isEnabledCheckBoxDisplayed = False
+            group4ChildInputs = group4CmdInput.children
+
+    
+            
+            i1 = groupChildInputs.addSelectionInput(SE01_SELECTION_INPUT_ID, SE01_SELECTION_INPUT_ID, "Select curve")
+            
             i1.addSelectionFilter(adsk.core.SelectionCommandInput.SketchCurves)
             i1.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
-            i2 = inputs.addSelectionInput(SE02_SELECTION_INPUT_ID, SE02_SELECTION_INPUT_ID, "Select curve")
+            i2 = groupChildInputs.addSelectionInput(SE02_SELECTION_INPUT_ID, SE02_SELECTION_INPUT_ID, "Select curve")
             i2.addSelectionFilter(adsk.core.SelectionCommandInput.SketchCurves)
-            i2.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
-            i3 = inputs.addSelectionInput(SE03_SELECTION_INPUT_ID, SE03_SELECTION_INPUT_ID, "Select curve")
+            #i2.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
+            i3 = groupChildInputs.addSelectionInput(SE03_SELECTION_INPUT_ID, SE03_SELECTION_INPUT_ID, "Select curve")
             i3.addSelectionFilter(adsk.core.SelectionCommandInput.SketchCurves)
-            i3.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
-            i4 = inputs.addValueInput(IN01_VALUE_INPUT_ID, '1/4 Punkte Eiform', '', adsk.core.ValueInput.createByReal(9))
-            i5 = inputs.addSelectionInput(SE04_SELECTION_INPUT_ID, SE04_SELECTION_INPUT_ID, "Select Plane")
+            #i3.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
+            i4 = group2ChildInputs.addValueInput(IN01_VALUE_INPUT_ID, '1/4 Punkte Eiform', '', adsk.core.ValueInput.createByReal(9))
+            i5 = groupChildInputs.addSelectionInput(SE04_SELECTION_INPUT_ID, SE04_SELECTION_INPUT_ID, "Select Plane")
             i5.addSelectionFilter(adsk.core.SelectionCommandInput.ConstructionPlanes)
-            i5.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
-            i6 = inputs.addValueInput(IN02_VALUE_INPUT_ID, IN01_VALUE_INPUT_ID, "mm", adsk.core.ValueInput.createByReal(0.5))
-            i7 = inputs.addValueInput(IN03_VALUE_INPUT_ID, IN03_VALUE_INPUT_ID, '', adsk.core.ValueInput.createByReal(0))
-            i8 = inputs.addBoolValueInput(CH01_BOOLEAN_INPUT_ID, "Erster Spant bei 0.0", True, '', False)
-            i9 = inputs.addStringValueInput(IN04_STRING_INPUT_ID, 'Offsetwerte, semikolongetrennt in mm', "")
+            #i5.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
+            i6 = group3ChildInputs.addValueInput(IN02_VALUE_INPUT_ID, IN01_VALUE_INPUT_ID, "mm", adsk.core.ValueInput.createByReal(0.5))
+            i7 = group3ChildInputs.addValueInput(IN03_VALUE_INPUT_ID, IN03_VALUE_INPUT_ID, '', adsk.core.ValueInput.createByString("0"))
+            i8 = group3ChildInputs.addBoolValueInput(CH01_BOOLEAN_INPUT_ID, "Erster Spant bei 0.0", True, '', False)
+            i9 = group3ChildInputs.addBoolValueInput(CH02_BOOLEAN_INPUT_ID, "Richtung invertieren", True, '', False)
+
+
+            tableInput = group4ChildInputs.addTableCommandInput('table', 'Table', 1, '1:0:0')
+            addRowToTable(tableInput)
+
+            # Add inputs into the table.            
+            addButtonInput = tab1ChildInputs.addBoolValueInput('tableAdd', 'Add', False, '', True)
+            tableInput.addToolbarCommandInput(addButtonInput)
+            deleteButtonInput = tab1ChildInputs.addBoolValueInput('tableDelete', 'Delete', False, '', True)
+            tableInput.addToolbarCommandInput(deleteButtonInput)
+
+            
+            
+            tabCmdInput2 = inputs.addTabCommandInput('tab_2', 'Tab 2')
+            tab2ChildInputs = tabCmdInput2.children
 
         except:
             if ui:
                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 
+
 def run(context):
     try:
-        title = 'Select Construction Plane'
+        global app, ui
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+        
+        
+        
+        title = 'Hügelschäffer Spans'
 
         if not design:
             ui.messageBox('No active Fusion design', title)
@@ -405,10 +519,9 @@ def run(context):
         commandDefinitions = ui.commandDefinitions
         
         # Get the existing command definition or create it if it doesn't already exist.
-        cmdDef = commandDefinitions.itemById("SpanCMDDef")
+        cmdDef = commandDefinitions.itemById(COMMAND_ID)
         if not cmdDef:
-            cmdDef = commandDefinitions.addButtonDefinition("SpanCMDDef",
-                                                            "Span Parameters", "Creates span spline on selected construction plane")
+            cmdDef = commandDefinitions.addButtonDefinition(COMMAND_ID, "Span Parameters", "Creates span spline on selected construction plane")
         
         # Connect to the command created event.
         onCommandCreated = SpanCommandCreatedHandler()
@@ -416,7 +529,7 @@ def run(context):
         handlers.append(onCommandCreated)
           
         # Execute the command definition.
-        inputs = adsk.core.NamedValues.create()
+        inputs = adsk.core.NamedValues.create()             ##### ?
         cmdDef.execute(inputs)
 
         # Prevent this module from being terminated when the script returns, because we are waiting for event handlers to fire.
